13f9c42ef75e674c41d6dac39670f5db2fc9170e
Usman Abdullahi <ua67527056@gmail.com>
2024-10-11 10:27:50 +0100

    Homepage CleanUp

--- src/CLIENT/Components/Portfolio.jsx
+++ src/CLIENT/Components/Portfolio.jsx
@@ -1,89 +1,3 @@
-// import React from "react";
-// import PortfolioCard from "./PortfolioCard";
-
-// const Portfolio = () => {
-//   const portfolioData = [
-//     {
-//       name: "Usman Abdullahi",
-//       profession: "Frontend Developer",
-//       img: "/image.png",
-//       comments:
-//         "Bitsan platform has been such a great experience using one of their services is top notch without any regrets. I was able to book my hostel through their website Bitsan hostel accommodations.",
-//     },
-//     {
-//       name: "Hussein Idris",
-//       profession: "Fullstack Developer",
-//       img: "/image.png",
-//       comments:
-//         "Bitsan platform has been such a great experience using one of their services is top notch without any regrets. I was able to book my hostel through their website Bitsan hostel accommodations.",
-//     },
-//     {
-//       name: "Favour Olowo",
-//       profession: "Student",
-//       img: "/image.png",
-//       comments:
-//         "Bitsan platform has been such a great experience using one of their services is top notch without any regrets. I was able to book my hostel through their website Bitsan hostel accommodations.",
-//     },
-//   ];
-//   return (
-//     <section className="relative pt-[7.5rem] flex flex-col gap-24 bg-[#F7F7F7] pb-11 lg:px-20 px-6">
-//       <img
-//         src="/SVGS/telephone.svg"
-//         className="absolute top-0 right-7 opacity-30"
-//         alt=""
-//       />
-//       <img
-//         src="/SVGS/telephone2.svg"
-//         className="absolute bottom-0 left-7 opacity-30"
-//         alt=""
-//       />
-//       <h1 className="font-semibold text-6xl text-center">
-//         What our clients <br /> has to say
-//       </h1>
-//       <div className="flex flex-col gap-[4.1875rem]">
-//         <div className="Portfolios flex justify-center items-center ">
-//           {portfolioData.map((portfolio) => (
-//             <PortfolioCard {...portfolio} />
-//           ))}
-//         </div>
-//         <div className="flex justify-between items-center">
-//           <button className="backward w-[65px] h-[65px] bg-primaryYellow opacity-0 rounded-full flex justify-center items-center">
-//             <i
-//               className="text-white text-2xl font-bold fa fa-chevron-left"
-//               aria-hidden="true"
-//             ></i>
-//           </button>
-
-//           <div className=".dots flex gap-2">
-//             <button className="w-[15px] h-[15px] bg-primaryYellow rounded-full"></button>
-//             <button className="w-[15px] h-[15px] bg-[#D9D9D9] rounded-full"></button>
-//             <button className="w-[15px] h-[15px] bg-[#D9D9D9] rounded-full"></button>
-//             <button className="w-[15px] h-[15px] bg-[#D9D9D9] rounded-full"></button>
-//             <button className="w-[15px] h-[15px] bg-[#D9D9D9] rounded-full"></button>
-//           </div>
-//           <button className="forward w-[65px] h-[65px] bg-primaryYellow rounded-full flex justify-center items-center">
-//             <i
-//               className="text-white text-2xl font-bold fa fa-chevron-right"
-//               aria-hidden="true"
-//             ></i>
-//           </button>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Portfolio;
-
-
-
-
-
-
-
-
-
-
 import React from "react";
 import PortfolioCard from "./PortfolioCard";
 import Slider from "react-slick";
@@ -140,12 +54,12 @@ const Portfolio = () => {
     <section className="relative pt-[7.5rem] flex flex-col gap-24 bg-[#F7F7F7] pb-11 lg:px-20 px-6">
       <img
         src="/SVGS/telephone.svg"
-        className="absolute top-0 right-7 opacity-30"
+        className="absolute top-0 right-7 opacity-10"
         alt=""
       />
       <img
         src="/SVGS/telephone2.svg"
-        className="absolute bottom-0 left-7 opacity-30"
+        className="absolute bottom-0 left-7 opacity-10"
         alt=""
       />
       <h1 className="font-semibold text-6xl text-center">
